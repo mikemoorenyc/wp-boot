7cee7186b98dd0cccd9702cfc2315c0c40b71b6e
mikeyfrecks <tigerhollowpoint@gmail.com>
2015-01-26 12:54:05 -0500

    Update header.php

--- header.php
+++ header.php
@@ -52,7 +52,7 @@ endwhile;
 else: ?>
 <?php echo $namer; ?>
 <?php endif; ?>" />
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
+
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
