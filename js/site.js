a0d313df2821be44feba4c785f50f887c893fe2c
Mike Moore <tigerhollowpoint@gmail.com>
2015-04-08 09:45:16 -0400

    Added in some global variables to the JS.

--- js/site.js
+++ js/site.js
@@ -1,8 +1,17 @@
-//GLOBALS
-siteDir = '/wp-content/themes/w25th-build'
-
-
 $(document).ready(function(){
+  //GLOBALS
+  siteDir = '/wp-content/themes/w25th-build',
+  ts = 250,
+  tab = 401,
+  dt = 801;
+  windoww = $(window).width();
+  windowh = $(window).height();
+  $(window).resize(function(){
+    windoww = $(window).width();
+    windowh = $(window).height();
+  });
+
+
   //LOAD IN EXPANDED CSS
   loadCSS(cssExpand);
   $('body').addClass('page-loaded');
