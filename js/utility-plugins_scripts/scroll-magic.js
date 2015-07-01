6505dd7a5db3fa88d28ea847962b4026ca30d561
Mike Moore <tigerhollowpoint@gmail.com>
2015-07-01 13:29:44 -0400

    more cleanup

--- js/utility-plugins_scripts/scroll-magic.js
+++ js/utility-plugins_scripts/scroll-magic.js
@@ -1,5 +1,5 @@
 function scrollMagic() {
-  //console.log('scroll');
+
 
 
 
@@ -9,7 +9,7 @@ function scrollMagic() {
 
   var stop = $(document).scrollTop();
 
-  
+
 
   $('.scroll-magic').each(function(){
     var theMagic = $(this);
@@ -17,34 +17,26 @@ function scrollMagic() {
 
       var destop = $(theMagic).offset(),
           dtop = destop.top;
-      //console.log(dtop+' '+stop);
+
       if ((dtop-200)-cutoff <= stop) {
-      //  console.log('asdfasfds');
+
         $(theMagic).addClass('__scroll-activated');
         scrollRunner(theMagic);
 
-      }/* else {
-        if (stop > (dtop-100)-cutoff && stop < (dtop+100)-cutoff) {
-          $(theMagic).addClass('__scroll-activated');
-          scrollRunner(theMagic);
-
-        }
-      }*/
+      }
     }
 
   });
   function scrollRunner(theMagic) {
-  //  console.log(theMagic);
+
     var thefunction = $(theMagic).data('scrollfunction'),
         thevariables = $(theMagic).data('scrollvariables');
     if (thevariables === "theObject") {
       thevariables = theMagic;
     }
     if (typeof window[thefunction] == 'function') {
-      //console.log(thefunction);
-      window[thefunction](thevariables);
-    } else {
 
+      window[thefunction](thevariables);
     }
   }
 }
