f57987e39a69aa071b2af11ce70912105af76229
Mike Moore <tigerhollowpoint@gmail.com>
2015-07-10 09:06:09 -0400

    added content cleaner function

--- functions.php
+++ functions.php
@@ -22,5 +22,21 @@ function dirReplacer($string) {
   $newString = str_replace('***TIMESTAMP***', $time ,$newString);
   echo $newString;
 }
+//CONTENT CLEANER
+function content_cleaner($content) {
+
+    // Remove inline styling
+    $content = preg_replace('/(<[^>]+) style=".*?"/i', '$1', $content);
+
+    // Remove font tag
+    $content = preg_replace('/<font[^>]+>/', '', $content);
+
+    // Remove empty tags
+    $post_cleaners = array('<p></p>' => '', '<p> </p>' => '', '<p>&nbsp;</p>' => '', '<span></span>' => '', '<span> </span>' => '', '<span>&nbsp;</span>' => '', '<span>' => '', '</span>' => '', '<font>' => '', '</font>' => '');
+    $content = strtr($content, $post_cleaners);
+
+    return $content;
+}
+
 
 ?>
