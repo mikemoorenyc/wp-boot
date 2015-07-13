fded3ce4d2f66c95cb935c17f091967141c7ea2e
Mike Moore <tigerhollowpoint@gmail.com>
2015-07-13 15:45:15 -0400

    Updated the functions.php file just to hold some code for adding shortcodes to the edit menu.

--- functions.php
+++ functions.php
@@ -37,6 +37,84 @@ function content_cleaner($content) {
 
     return $content;
 }
+/*
+add_action( 'admin_menu', 'short_coder' );
+add_action( 'load-post-new.php', 'short_coder' );
 
+
+function short_coder() {
+
+
+  add_action( 'add_meta_boxes', 'shortcode_add' );
+}
+
+
+function shortcode_add() {
+$post_types = get_post_types();
+
+  foreach($post_types as $pt) {
+    add_meta_box(
+      'shortcode-link',      // Unique ID
+      esc_html__( 'Shortcode' ),    // Title
+      'shortcode_html',   // Callback function
+      $pt,
+      'side',         // Context
+      'default'         // Priority
+    );
+  }
+
+
+}
+
+function shortcode_html() {
+  ?>
+  <div id="shortcode-display">
+	<?php
+		$pid = $GLOBALS['post_ID'];
+		$type = get_post_type( $pid );
+
+		if ($title != 'Auto Draft'):
+	?>
+		<p>Copy and paste this shortcode into any Post, Page or even another Bucket. Yeah that's right... a Bucket within a Bucket.</p>
+		<div class="bucket-shortcode">
+			<input type="text" readonly value='[<?php echo $type;?> id="<?php echo $pid; ?>"]' onFocus="this.select()" />
+		</div>
+	<?php else: ?>
+		<p>Save the Bucket and a shortcode will be generated.</p>
+	<?php endif; ?>
+</div>
+
+  <?php
+}
+
+
+// ADD NEW COLUMN
+function columns_head($defaults) {
+    $defaults['shorty'] = 'SHORTCODE';
+    return $defaults;
+}
+
+// SHOW THE FEATURED IMAGE
+function column_content($column_name, $post_ID) {
+    if ($column_name == 'shorty') {
+        $pid = $post_ID;
+        $type = get_post_type( $pid );
+
+        ?>
+<input type="text" readonly value='[<?php echo $type;?> id="<?php echo $pid; ?>"]' onFocus="this.select()" />
+        <?php
+    }
+}
+
+
+add_filter('manage_posts_columns', 'columns_head');
+add_action('manage_posts_custom_column', 'column_content', 10, 2);
+
+add_filter('manage_pages_columns', 'columns_head');
+add_action('manage_pages_custom_column', 'column_content', 10, 2);
+
+
+
+*/
 
 ?>
