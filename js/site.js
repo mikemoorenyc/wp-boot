e2bb58cee0ae2f06982e16f4a452f14ffa7ae58b
Mike Moore <tigerhollowpoint@gmail.com>
2015-10-05 15:48:26 -0400

    adding in fastclick automatically

--- js/site.js
+++ js/site.js
@@ -1,4 +1,11 @@
 function siteInit() {
+  if ('addEventListener' in document) {
+      document.addEventListener('DOMContentLoaded', function() {
+          FastClick.attach(document.body);
+      }, false);
+  }
+  
+  
   //GLOBALS
   ts = 250,
   tab = 401,
