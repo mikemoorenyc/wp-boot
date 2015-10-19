1eedf5e34baf94e0b24aa1058ee55cb001549a95
Mike Moore <tigerhollowpoint@gmail.com>
2015-10-19 14:38:07 -0400

    reformatted the gulpfile for better readability

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,34 @@
 var buildDir = 'build-new';
-//
-var gulp = require('gulp'),
-  htmlclean = require('gulp-htmlclean'),
-  uglify = require('gulp-uglify'),
-  concat = require('gulp-concat'),
-  imagemin = require('gulp-imagemin'),
-  jshint = require('gulp-jshint'),
-  pngcrush = require('imagemin-pngcrush'),
-  svgstore = require('gulp-svgstore'),
-  changed = require('gulp-changed');
 
-//CSS STACK
+//GENERAL MODULES
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    changed = require('gulp-changed');
+
+//CSS PROCESSING
 var sass = require('gulp-sass'),
     minifyCSS = require('gulp-minify-css'),
     postcss = require('gulp-postcss'),
     mqpacker = require('css-mqpacker'),
-    autoprefixer = require('autoprefixer')
+    autoprefixer = require('autoprefixer');
 
-gulp.task('svgstore', function () {
-    return gulp
-        .src('assets/svgs/*.svg')
-        .pipe(imagemin())
-        .pipe(svgstore({ inlineSvg: true }))
-        .pipe(gulp.dest('../'+buildDir+'/assets'));
+gulp.task('sass', function () {
+
+  var processors = [
+    autoprefixer({ browsers: ['last 2 versions'] }),
+    mqpacker
+  ];
+
+  gulp.src(['sass/main.scss', 'sass/expanded.scss','sass/ie-fixes.scss','sass/editor-styles.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(minifyCSS({keepBreaks:false, keepSpecialComments: 0}))
+    .pipe(gulp.dest('../'+buildDir+'/css'));
 });
+
+//JAVASCRIPT PROCESSING
+var uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint');
 
 gulp.task('js', function () {
   gulp.src([ 'js/plugins/*.js', 'js/site.js', 'js/modules/*.js'])
@@ -37,28 +42,14 @@ gulp.task('js', function () {
     .pipe(gulp.dest('../'+buildDir+'/js'));
 });
 
-gulp.task('sass', function () {
-
-  var processors = [
-    autoprefixer({ browsers: ['last 2 versions'] }),
-    mqpacker
-  ];
-
-  gulp.src(['sass/main.scss', 'sass/expanded.scss','sass/ie-fixes.scss','editor-styles.scss'])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(processors))
-    .pipe(minifyCSS({keepBreaks:false, keepSpecialComments: 0}))
-    .pipe(gulp.dest('../'+buildDir+'/css'));
-})
-
-
-
-gulp.task('imgmin', function () {
-  gulp.src('assets/imgs/**/*')
-    .pipe(changed('../'+buildDir+'/assets/imgs'))
-    .pipe(imagemin({interlaced: true, progressive: true,svgoPlugins: [{removeViewBox: false}],use: [pngcrush()]}))
-    .pipe(gulp.dest('../'+buildDir+'/assets/imgs'));
+gulp.task('lint', function() {
+  return gulp.src(['js/site.js', 'modules/*.js', 'js/inline-load.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
+
+//HTML PROCESSING
+var  htmlclean = require('gulp-htmlclean');
 
 gulp.task('templatecrush', function() {
   gulp.src(['*.php','*.html','!custom-module-functions.php'])
@@ -67,8 +58,27 @@ gulp.task('templatecrush', function() {
     .pipe(gulp.dest('../'+buildDir));
 });
 
+//IMAGE PROCESSING
+var pngcrush = require('imagemin-pngcrush'),
+    svgstore = require('gulp-svgstore'),
+    imagemin = require('gulp-imagemin');
 
+gulp.task('svgstore', function () {
+    return gulp
+        .src('assets/svgs/*.svg')
+        .pipe(imagemin())
+        .pipe(svgstore({ inlineSvg: true }))
+        .pipe(gulp.dest('../'+buildDir+'/assets'));
+});
 
+gulp.task('imgmin', function () {
+  gulp.src('assets/imgs/**/*')
+    .pipe(changed('../'+buildDir+'/assets/imgs'))
+    .pipe(imagemin({interlaced: true, progressive: true,svgoPlugins: [{removeViewBox: false}],use: [pngcrush()]}))
+    .pipe(gulp.dest('../'+buildDir+'/assets/imgs'));
+});
+
+//DUMPS
 gulp.task('fontdump', function(){
   gulp.src('assets/fonts/**/*')
     .pipe(gulp.dest('../'+buildDir+'/assets/fonts'));
@@ -79,18 +89,9 @@ gulp.task('wpdump', function(){
     .pipe(gulp.dest('../'+buildDir));
 });
 
-gulp.task('lint', function() {
-  return gulp.src(['js/site.js', 'modules/*.js', 'js/inline-load.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
-
-
-
 gulp.task('watch', function() {
     gulp.watch('js/**/*.js', ['js']);
-    gulp.watch(['sass/**/*', 'editor-styles.scss'], ['sass']);
+    gulp.watch(['sass/**/*'], ['sass']);
     gulp.watch('assets/imgs/**/*', ['imgmin']);
     gulp.watch('assets/fonts/**/*', ['fontdump']);
     gulp.watch(['*.php', '*.html'], ['templatecrush']);
