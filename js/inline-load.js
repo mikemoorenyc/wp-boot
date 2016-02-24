3ae6cf6ef9af47e802b547f36ab9256d0b28ff8e
Mike Moore <tigerhollowpoint@gmail.com>
2016-02-23 21:59:57 -0500

    global namespace stuff

--- js/inline-load.js
+++ js/inline-load.js
@@ -1,10 +1,9 @@
-//DECLARE THE GLOBAL APP HERE
-var app = {},
-app.siteDir = '***REPLACEWITHTHEMEDIRECTORY***';
 
-app.timestamp = '***TIMESTAMP***';
+//GLOBAL JAVASCRIPT VARIABLES TAKEN FROM PHP
+var phpvars_siteDir = '***REPLACEWITHTHEMEDIRECTORY***',
+    phpvars_timestamp = '***TIMESTAMP***';
 
-cssExpand = siteDir+"/css/expanded.css?v="+app.timestamp;
+var cssExpand = phpvars_siteDir+"/css/expanded.css?v="+phpvars_timestamp;
 
 function loadCSS(e,t,n){"use strict";function o(){var t;for(var i=0;i<s.length;i++){if(s[i].href&&s[i].href.indexOf(e)>-1){t=true}}if(t){r.media=n||"all"}else{setTimeout(o)}}var r=window.document.createElement("link");var i=t||window.document.getElementById("inline-scripts");var s=window.document.styleSheets;r.rel="stylesheet";r.href=e;r.media="only x";i.parentNode.insertBefore(r,i);o();return r}
 
