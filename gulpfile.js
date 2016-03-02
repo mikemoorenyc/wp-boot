a83f01f1c8cda55e16c885dcf1e2bfa76abc47ee
mikeyfrecks <tigerhollowpoint@gmail.com>
2016-03-01 22:07:01 -0500

    trying to update to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -27,16 +27,26 @@ var pngcrush = require('imagemin-pngcrush'),
     svgstore = require('gulp-svgstore'),
     imagemin = require('gulp-imagemin');
 
+var del = require('del'); // rm -rf
+//
+
+function clean() {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  // If you are using del 2.0 or above, return its promise
+  return del(['../'+buildDir+'/**'], {force:true});
+}
+
+
 //Generic sass Processor
 function sassProcessor(blob, dest) {
-  gulp.src(blob)
+   gulp.src(blob)
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(postcssprocessors))
     .pipe(gulp.dest(dest));
 }
 //Generic js Processor
 function jsProcessor(blob, dest, newName) {
-  gulp.src(blob)
+  return gulp.src(blob)
     .pipe(uglify())
     .on('error', console.error.bind(console))
     .pipe(concat(newName))
@@ -44,7 +54,7 @@ function jsProcessor(blob, dest, newName) {
 }
 //Generic html Processor
 function htmlProcessor(blob, dest) {
-  gulp.src(blob)
+  return gulp.src(blob)
     .pipe(changed(dest))
     .pipe(htmlclean({}))
     .pipe(gulp.dest(dest));
@@ -87,21 +97,23 @@ gulp.task('svgstore', function () {
 
 //IMAGE MINIFYING TASK
 gulp.task('imgmin', function () {
-  gulp.src('assets/imgs/**/*')
+  return gulp.src('assets/imgs/**/*')
     .pipe(changed('../'+buildDir+'/assets/imgs'))
     .pipe(imagemin({interlaced: true, progressive: true,svgoPlugins: [{removeViewBox: false}],use: [pngcrush()]}))
     .pipe(gulp.dest('../'+buildDir+'/assets/imgs'));
 });
 
 //DUMPS
+function dumper(src, dest) {
+  return gulp.src(src)
+    .pipe(gulp.dest(dest));
+}
 gulp.task('fontdump', function(){
-  gulp.src('assets/fonts/**/*')
-    .pipe(gulp.dest('../'+buildDir+'/assets/fonts'));
+  dumper('assets/fonts/**/*', '../'+buildDir+'/assets/fonts');
 });
 
 gulp.task('wpdump', function(){
-  gulp.src(['style.css', 'screenshot.png'])
-    .pipe(gulp.dest('../'+buildDir));
+  dumper(['style.css', 'screenshot.png'], '../'+buildDir);
 });
 
 gulp.task('watch', function() {
@@ -113,4 +125,11 @@ gulp.task('watch', function() {
     gulp.watch(['style.css', 'screenshot.png'], ['wpdump']);
     gulp.watch(['assets/svgs/*.svg'], ['svgstore']);
 });
+
 gulp.task('build', [ 'js', 'imgmin', 'templatecrush', 'fontdump', 'wpdump','sass', 'svgstore']);
+/*
+gulp.task('build', gulp.series(
+  "clean",
+  gulp.parallel( 'js', 'imgmin', 'templatecrush', 'fontdump', 'wpdump','sass', 'svgstore')
+));
+*/
