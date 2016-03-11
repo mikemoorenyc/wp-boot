78a3aafd1d84aa91d1fe7b3de18cc64629bd54bf
Mike Moore <tigerhollowpoint@gmail.com>
2016-03-11 11:02:40 -0500

    delete IE8 conditionals

--- header.php
+++ header.php
@@ -68,13 +68,6 @@ else: ?>
 <?php wp_site_icon();?>
 
 
-<!--  STUFF FOR IE8 WILL GET REMOVED ON COMPILATION // REMOVE THIS LINE TO RENDER IT
-<!--[if lte IE 8]>
-<link rel="stylesheet" href="<?php echo $siteDir;?>/css/expanded.css" />
-	<link href='<?php echo $siteDir;?>/css/ie-fixes.css?ts=<?php echo time();?>' rel='stylesheet' type='text/css'>
-  	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
 
 
 <!-- FACEBOOK & TWiTTER TAGS REMOVED ON COMPILATION UNLESS YOU UNCOMMENT-->
